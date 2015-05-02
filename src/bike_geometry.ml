0153baff446701156bf8f51796f42abf9062b6e1
John Else <john.else@citrix.com>
2015-05-02 04:21:29 +0100

    Start writing the renderer
    Signed-off-by: John Else <john.else@citrix.com>

--- src/bike_geometry.ml
+++ src/bike_geometry.ml
@@ -1,10 +1,26 @@
 module Html = Dom_html
 
+let pi = 4. *. atan 1.
+
 type geometry = {
-  head_angle : float;
-  seat_angle : float;
-  wheel_size : float;
-  wheelbase : float;
+  head_angle   : float;
+  seat_angle   : float;
+  wheel_radius : float;
+  wheel_base   : float;
+}
+
+let scale_factor = 4.0
+
+let scale geometry = {geometry with
+  wheel_radius = geometry.wheel_radius /. scale_factor;
+  wheel_base   = geometry.wheel_base   /. scale_factor;
+}
+
+let test_geometry = scale {
+  head_angle   = 65.5;
+  seat_angle   = 72.0;
+  wheel_radius = 340.0;
+  wheel_base   = 1103.0;
 }
 
 let width = 600
@@ -16,10 +32,44 @@ let create_canvas width height =
   canvas##height <- height;
   canvas
 
+let render ctx geometry =
+  (* Front wheel. *)
+  ctx##moveTo (
+    2. *. geometry.wheel_radius,
+    (float height) -. geometry.wheel_radius
+  );
+  ctx##arc (
+    geometry.wheel_radius,
+    (float height) -. geometry.wheel_radius,
+    geometry.wheel_radius,
+    0.,
+    2. *. pi,
+    Js._true
+  );
+  ctx##stroke ();
+  (* Rear wheel. *)
+  ctx##moveTo (
+    2. *. geometry.wheel_radius +. geometry.wheel_base,
+    (float height) -. geometry.wheel_radius
+  );
+  ctx##arc (
+    geometry.wheel_radius +. geometry.wheel_base,
+    (float height) -. geometry.wheel_radius,
+    geometry.wheel_radius,
+    0.,
+    2. *. pi,
+    Js._true
+  );
+  ctx##stroke ()
 
 let start _ =
   let canvas = create_canvas width height in
   Dom.appendChild Html.document##body canvas;
+  let ctx = canvas##getContext (Html._2d_) in
+  ctx##strokeStyle <- (Js.string "#000000");
+  ctx##clearRect (0., 0., float width, float height);
+  render ctx test_geometry;
+  ctx##stroke ();
   Js._false
 
 let _ =
